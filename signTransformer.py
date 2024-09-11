db8bc8b04bc8ccdab85598da2602bccd375e3345
Yiming Ni <yimingni@Yimings-MacBook-Pro.local>
2024-09-11 12:39:01 -0700

    update signTransformer

--- signTransformer.py
+++ signTransformer.py
@@ -34,6 +34,21 @@ class AddNorm(nn.Module):
 
 ############################################################
 # Encoder ##################################################
+class TransformerEncoderBlock(nn.Module):
+    """The Transformer encoder block."""
+    def __init__(self, num_hiddens, ffn_num_hiddens, num_heads, dropout,
+                 use_bias=False):
+        super().__init__()
+        self.attention = d2l.MultiHeadAttention(num_hiddens, num_heads,
+                                                dropout, use_bias)
+        self.addnorm1 = AddNorm(num_hiddens, dropout)
+        self.ffn = PositionWiseFFN(ffn_num_hiddens, num_hiddens)
+        self.addnorm2 = AddNorm(num_hiddens, dropout)
+
+    def forward(self, X, valid_lens):
+        Y = self.addnorm1(X, self.attention(X, X, X, valid_lens))
+        return self.addnorm2(Y, self.ffn(Y))
+        
 class TransformerEncoder_NoEmbedding(d2l.Encoder):
     def __init__(self, num_hiddens, ffn_num_hiddens,
                  num_heads, num_blks, dropout, use_bias=False):
